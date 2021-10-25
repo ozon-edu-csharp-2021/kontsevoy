0e670d9d1fd2e3cf2198f1afe6cb465dfb3267d1
Alexander Kontsevoy <alexanderserv@bk.ru>
2021-10-25 19:23:50 +0500

    Add version, ready and live middlewares as infrastructure

---
+++ src/MerchandiseService/Infrastructure/Middlewares/VersionMiddleware.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace MerchandiseService.Infrastructure.Middlewares
+{
+    public class VersionMiddleware
+    {
+        public VersionMiddleware(RequestDelegate next) {}
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var assembly = Assembly.GetExecutingAssembly().GetName();
+            await context.Response.WriteAsync($"{{\"version\": \"{assembly.Version}\", \"serviceName\": \"{assembly.Name}\"}}");
+        }
+    }
+}
--- src/MerchandiseService/Program.cs
+++ src/MerchandiseService/Program.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using MerchandiseService;
+using MerchandiseService.Infrastructure.Extensions;
 
 CreateHostBuilder(args).Build().Run();
 
 static IHostBuilder CreateHostBuilder(string[] args) =>
     Host.CreateDefaultBuilder(args)
-        .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
+        .AddInfrastructure();
