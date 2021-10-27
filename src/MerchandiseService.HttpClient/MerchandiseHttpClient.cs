6d315e44dfcbca753216e9a484e0a982cdb0a96d
Alexander Kontsevoy <alexanderserv@bk.ru>
2021-10-28 02:12:05 +0500

    Add RestEase

--- src/MerchandiseService.HttpClient/MerchandiseHttpClient.cs
+++ src/MerchandiseService.HttpClient/MerchandiseHttpClient.cs
@@ -1,38 +1,16 @@
-﻿using System.Net.Http;
-using System.Text;
-using System.Text.Json;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using MerchandiseService.HttpClient.Models;
-using SysHttpClient = System.Net.Http.HttpClient;
+using RestEase;
 
 namespace MerchandiseService.HttpClient
 {
+    [BasePath("v1/api/merch")]
     public interface IMerchandiseHttpClient
     {
-        Task<RequestMerchResponse> RequestMerch(RequestMerchRequest request, CancellationToken token);
-        Task<InquiryMerchResponse> InquiryMerch(InquiryMerchRequest request, CancellationToken token);
-    }
-    
-    public class MerchandiseHttpClient : IMerchandiseHttpClient
-    {
-        private SysHttpClient HttpClient { get; }
-
-        public MerchandiseHttpClient(SysHttpClient httpClient) => HttpClient = httpClient;
-
-        private async Task<R> PostAsync<T, R>(string requestUri, T request, CancellationToken token)
-        {
-            var requestBody = JsonSerializer.Serialize(request);
-            var requestContent = new StringContent(requestBody, Encoding.UTF8, "application/json");
-            using var response = await HttpClient.PostAsync(requestUri, requestContent, token);
-            var body = await response.Content.ReadAsStringAsync(token);
-            return JsonSerializer.Deserialize<R>(body);
-        }
-
-        public async Task<RequestMerchResponse> RequestMerch(RequestMerchRequest request, CancellationToken token) =>
-            await PostAsync<RequestMerchRequest, RequestMerchResponse>("v1/api/merch/request", request, token);
-
-        public async Task<InquiryMerchResponse> InquiryMerch(InquiryMerchRequest request, CancellationToken token) =>
-            await PostAsync<InquiryMerchRequest, InquiryMerchResponse>("v1/api/merch/inquiry", request, token);
+        [Post("request")]
+        Task<RequestMerchResponse> RequestMerch([Body] RequestMerchRequest request, CancellationToken token);
+        [Post("inquiry")]
+        Task<InquiryMerchResponse> InquiryMerch([Body] InquiryMerchRequest request, CancellationToken token);
     }
 }
