77bcc7b2ec9143f29f7d91940f02be21205f69bf
Alexander <Alexander.Koncevoy@gmail.com>
2021-11-07 23:30:28 +0500

    Email ValueObject tests

---
+++ tests/MerchandiseService.Domain.Tests/EmailValueObjectTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MerchandiseService.Domain.AggregationModels.ValueObjects;
+using Xunit;
+
+namespace MerchandiseService.Domain.Tests
+{
+    public class EmailValueObjectTests
+    {
+        [Fact(DisplayName = "Email - Плохие адреса")]
+        public void SomeBadEmails()
+        {
+            Assert.Throws<ArgumentException>(() => new Email("some bad email"));
+            Assert.Throws<ArgumentException>(() => new Email("some bad e.mail"));
+            Assert.Throws<ArgumentException>(() => new Email("some bad e-mail"));
+        }
+        
+        [Fact(DisplayName = "Email - Хорошие адреса")]
+        public void SomeGoodEmails()
+        {
+            var email = "some+good@e-mail.ru";
+            Assert.Equal(email, new Email(email).Value);
+            email = "other@good.email";
+            Assert.Equal(email, new Email(email).Value);
+        }
+    }
+}
