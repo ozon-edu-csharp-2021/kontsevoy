c892ef8c61e460d4b15f8f5c3513fc21cff4fc84
Alexander <Alexander.Koncevoy@gmail.com>
2021-11-07 23:44:33 +0500

    EmployeeId test

---
+++ tests/MerchandiseService.Domain.Tests/EmployeeIdValueObjectTests.cs
@@ -0,0 +1,15 @@
+﻿using MerchandiseService.Domain.AggregationModels.ValueObjects;
+using Xunit;
+
+namespace MerchandiseService.Domain.Tests
+{
+    public class EmployeeIdValueObjectTests
+    {
+        [Fact(DisplayName = "Значение сохраняется")]
+        public void EmployeeIdValues()
+        {
+            const ulong value = 73284687623UL;
+            Assert.Equal(value, new EmployeeId(value).Value);
+        }
+    }
+}
