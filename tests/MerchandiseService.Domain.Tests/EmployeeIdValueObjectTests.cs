e4d431342c2544785d664c7a655065743d4b725a
Alexander <Alexander.Koncevoy@gmail.com>
2021-11-08 22:19:35 +0500

    Other EmployeeId value object tests

--- tests/MerchandiseService.Domain.Tests/EmployeeIdValueObjectTests.cs
+++ tests/MerchandiseService.Domain.Tests/EmployeeIdValueObjectTests.cs
@@ -11,5 +11,14 @@ namespace MerchandiseService.Domain.Tests
             const ulong value = 73284687623UL;
             Assert.Equal(value, new EmployeeId(value).Value);
         }
+        
+        [Fact(DisplayName = "Равенство экземпляров")]
+        public void Equality()
+        {
+            Assert.Equal(new EmployeeId(0), new EmployeeId(0));
+            Assert.Equal(new EmployeeId(982347983479), new EmployeeId(982347983479));
+            Assert.NotEqual(new EmployeeId(0), new EmployeeId(982347983479));
+            Assert.NotEqual(new EmployeeId(0), new EmployeeId(1));
+        }
     }
 }
