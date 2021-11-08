7f3f97c5d1f6056fa20b7f81750d96f85a4e9437
Alexander <Alexander.Koncevoy@gmail.com>
2021-11-08 22:47:45 +0500

    MerchRequest entity tests

--- src/MerchandiseService.Domain/AggregationModels/MerchRequestAggregate/MerchRequest.cs
+++ src/MerchandiseService.Domain/AggregationModels/MerchRequestAggregate/MerchRequest.cs
@@ -1,4 +1,5 @@
-﻿using MerchandiseService.Domain.AggregationModels.Enumerations;
+﻿using System;
+using MerchandiseService.Domain.AggregationModels.Enumerations;
 using MerchandiseService.Domain.AggregationModels.ValueObjects;
 using MerchandiseService.Domain.Models;
 
@@ -6,8 +7,13 @@ namespace MerchandiseService.Domain.AggregationModels.MerchRequestAggregate
 {
     public class MerchRequest : Entity<MerchRequestId>
     {
-        public MerchRequest(EmployeeId employeeId, MerchPack merchPack) =>
-            (EmployeeId, MerchPack) = (employeeId, merchPack);
+        public MerchRequest(EmployeeId employeeId, MerchPack merchPack)
+        {
+            EmployeeId = employeeId ?? throw new ArgumentNullException(nameof(employeeId), 
+                $"{nameof(employeeId)} must be provided");
+            MerchPack = merchPack ?? throw new ArgumentNullException(nameof(merchPack), 
+                $"{nameof(merchPack)} must be provided");
+        }
         
         public EmployeeId EmployeeId { get; }
         public MerchPack MerchPack { get; }
